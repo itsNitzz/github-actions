2f45699cf8d63bd1fb0b55038b9bdef2ba22f9ea
Nityanand Rai <nityanand_rai@epam.com>
2026-06-18 21:05:05 +0530

    17-2-custom-actions: add code to custom action to raise PR

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core");
 const exec = require("@actions/exec");
+const github = require("@actions/github");
 
 const validateBranchName = ({ branchName }) =>
   /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
@@ -11,7 +12,7 @@ async function run() {
   const targetBranch = core.getInput("target-branch");
   const workingDirectory = core.getInput("working-directory");
   const debug = core.getInput("debug");
-  const ghToken = core.getInput(" gh-token");
+  const ghToken = core.getInput("gh-token", { required: true });
 
   core.setSecret(ghToken);
 
@@ -52,6 +53,41 @@ async function run() {
 
   if (gitStatus.stdout.length > 0) {
     core.info("dependencies have been updated.");
+
+    await exec.exec(`git config --global user.email "gh-automation@gmail.com"`);
+    await exec.exec(`git config --global user.name "Nityanand Rai"`);
+
+    await exec.exec(`git switch -c ${targetBranch}`, [], {
+      cwd: workingDirectory,
+    });
+    await exec.exec(`git add package.json package-lock.json`, [], {
+      cwd: workingDirectory,
+    });
+    await exec.exec(`git commit -m "update package dependencies"`, [], {
+      cwd: workingDirectory,
+    });
+    await exec.exec(`git push -u origin ${targetBranch} -f`, [], {
+      cwd: workingDirectory,
+    });
+
+    const octokit = github.getOctokit(ghToken);
+
+    try {
+      await octokit.rest.pulls.create({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        title: `Update NPM dependencies`,
+        body: `This pull request updates NPM packages`,
+        base: baseBranch,
+        head: targetBranch,
+      });
+    } catch (e) {
+      core.error(
+        "[js-dependency-update] : Something went wrong while creating the PR. Check logs below.",
+      );
+      core.setFailed(e.message);
+      core.error(e);
+    }
   }
 
   core.info("I am a custom JS action");
